b9f4a75017e3b5d8908e3fae3464ca9ed7aa8d58
Nelson David Quiñones Virgen <44851531+bit2424@users.noreply.github.com>
2022-10-07 14:19:25 -0500

    Add translation from Mesh Terms to Semantic Types.
    Add translation from Mesh Terms to Semantic Types.

--- code/DataSet_Creation/Add_SemanticTypes.py
+++ code/DataSet_Creation/Add_SemanticTypes.py
@@ -1,2 +1,58 @@
+import os
+import pandas as pd
 
 
+'''
+    Function to Add Semantic types column, with the translation from Mesh terms to semantic types. 
+
+    Input:  df -> A Pandas Dataframe with 3 columns  "PMID", "Title/Abstract", "MeshTerms"
+            dic_MeshToSemantic -> A dictionary with the translation of the Mesh terms to Semantic Types
+    Output: A Pandas Dataframe with 4 columns  "PMID", "Title/Abstract", "MeshTerms", "SemanticTypes"
+'''
+
+def add_SemanticTypes(df,dic_MeshToSemantic):
+    df_out = pd.DataFrame(columns =  ["PMID", "Title/Abstract", "MeshTerms","SemanticTypes"])
+    i = 0
+    for ind in df.index:
+        mesh_list = df['MeshTerms'][ind].split(';')
+        semantic_list = set()
+        for mesh in mesh_list:
+            for semantic in dic_MeshToSemantic[mesh]:
+                semantic_list.add(semantic)
+        semanticTypes = ";".join(semantic_list)
+        print(semanticTypes)
+        df_out.loc[len(df_out)] = [df['PMID'][ind], df['Title/Abstract'][ind],df['MeshTerms'][ind],semanticTypes]
+    
+
+    return df_out
+
+'''
+    Function to process multiple Dataframes to enrich them with semantic types  
+
+    Input:  path -> A folder with all the fragments of datasets
+    Output: A series of tsv files with 4 columns  "PMID", "Title/Abstract", "MeshTerms", "SemanticTypes"
+'''
+
+def process_articles(path_in,path_out):
+    i = 0
+    df_MeshToSemantic = pd.read_csv("data//Mesh_datasets//Mesh_to_Semantic.csv")
+    print(df_MeshToSemantic.head())
+    dic_MeshToSemantic = {}
+    for data in df_MeshToSemantic.to_dict('records'):
+        dic_MeshToSemantic[data['Mesh id']] = str(data['Semantic Types']).split(',')
+    
+    #print(dic_MeshToSemantic)
+
+    for filename in os.listdir(path_in):
+        if filename.split('.')[-1] == "tsv":
+            print(filename)
+            df = pd.read_csv(path_in+"//"+filename,sep='\t')
+            df = df.reset_index()
+            df_out = add_SemanticTypes(df,dic_MeshToSemantic)
+            df_out.to_csv(path_out +"//"+filename.split('.')[0] + "_2.tsv")
+            print(df_out.head())
+        if(i == 0): break
+            
+
+
+process_articles("data//phase1","data//phase2")
