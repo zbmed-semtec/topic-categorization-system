07c0d2a188bc863c62cb24831f52d8e08e688358
Nelson David Quiñones Virgen <nelsonquinones2424@gmail.com>
2022-10-09 11:40:05 -0500

    Implemented Process Pool in Phase2

--- code/DataSet_Creation/Phase2_Add_SemanticTypes.py
+++ code/DataSet_Creation/Phase2_Add_SemanticTypes.py
@@ -1,8 +1,9 @@
 import os
 import time
 import pandas as pd
+import multiprocessing as mp
 from threading import Thread
-
+from multiprocessing import Pool
 
 dic_MeshToSemantic = {}
 
@@ -20,6 +21,13 @@ def loadMeshToSemanticDic():
     
     for data in df_MeshToSemantic.to_dict('records'):
         dic_MeshToSemantic[data['Mesh id']] = str(data['Semantic Types']).split(',')
+    
+    #print(dic_MeshToSemantic)
+
+def init_pool(the_dict):
+        global globalDict
+        globalDict = the_dict
+        print("Initialized")
 
 '''
     Function to Add Semantic types column, with the translation from Mesh terms to semantic types. 
@@ -30,14 +38,17 @@ def loadMeshToSemanticDic():
 '''
 
 def add_SemanticTypes(df):
+    global globalDict
     df_out = pd.DataFrame(columns =  ["PMID", "Title/Abstract", "MeshTerms","SemanticTypes"])
 
     for ind in df.index:
         mesh_list = df['MeshTerms'][ind].split(';')
         semantic_list = set()
         for mesh in mesh_list:
-            for semantic in dic_MeshToSemantic[mesh]:
+            #if mesh in dic_MeshToSemantic:
+            for semantic in globalDict[mesh]:
                 semantic_list.add(semantic)
+                
         semanticTypes = ";".join(semantic_list)
         #print(semanticTypes)
         df_out.loc[len(df_out)] = [df['PMID'][ind], df['Title/Abstract'][ind],df['MeshTerms'][ind],semanticTypes]
@@ -48,19 +59,20 @@ def add_SemanticTypes(df):
 '''
     Function to process a Dataframes to enrich them with semantic types  
 
-    Input:  filename -> The name of the .tsv to be processed 
-            path_in -> A folder with all the fragments of datasets
-            path_out -> The folder where all the augmented fragments are going to be stored
+    Input:  info_dic -> A dictionary that contains all the information needed to process an article
+                filename -> The name of the .tsv to be processed 
+                path_in -> A folder with all the fragments of datasets
+                path_out -> The folder where all the augmented fragments are going to be stored
             
     Output: A series of tsv files with 4 columns  "PMID", "Title/Abstract", "MeshTerms", "SemanticTypes"
 '''
 
-def process_article(filename,path_in,path_out):
-    print(filename)
-    df = pd.read_csv(path_in+"//"+filename,sep='\t')
+def     process_article(info_dic):
+    print(info_dic['filename'])
+    df = pd.read_csv(info_dic['path_in']+"//"+info_dic['filename'],sep='\t')
     df = df.reset_index()
     df_out = add_SemanticTypes(df)
-    df_out.to_csv(path_out +"//"+filename.split('.')[0] + "_2.tsv",sep="\t")
+    df_out.to_csv(info_dic['path_out'] +"//"+info_dic['filename'].split('.')[0] + "_2.tsv",sep="\t")
     print(df_out.head())
 
 '''
@@ -75,12 +87,12 @@ def process_article(filename,path_in,path_out):
 
 def process_articles_sequential(path_in,path_out):
     i = 0
-    
+    print("lol")
     #print(dic_MeshToSemantic)
 
     for filename in os.listdir(path_in):
         if filename.split('.')[-1] == "tsv":
-            process_article(filename,path_in,path_out)
+            process_article({"filename":filename,"path_in":path_in,"path_out":path_out})
             i+=1
             if(i == 6): break
 
@@ -98,40 +110,37 @@ def process_articles_concurrent(path_in,path_out):
     i = 0
     #print(dic_MeshToSemantic)
 
-    thread_size = 2
-    my_threads = []
+    # thread_size = 2
+    # my_threads = []
 
     dataset_List = []
 
     for filename in os.listdir(path_in):
         if filename.split('.')[-1] == "tsv":
-            dataset_List.append(filename)
+            dataset_List.append({"filename":filename,"path_in":path_in,"path_out":path_out})
             i+=1
-            if(i == 6): break
+            if(i == 8): break
     
-    for filename in dataset_List:
-            
-            if len(my_threads) < thread_size:
-                #print("This is f: ",f)
-                new_thread = Thread(target=process_article, args=(filename,path_in,path_out))
-                new_thread.start()
-                my_threads.append(new_thread)
-            else:
-                while len(my_threads) == thread_size:
-                    #print(f)
-                    #time.sleep(0.1)
-                    my_threads = [thread for thread in my_threads if thread.is_alive()]
-                new_thread = Thread(target=process_article, args=(filename,path_in,path_out))
-                new_thread.start()
-                my_threads.append(new_thread)         
+    pool = mp.Pool(4, initializer= init_pool, initargs = (dic_MeshToSemantic,))
+
+    try:
+        pool.map(process_article, dataset_List)
+    finally:
+        pool.close()
+        pool.join()
+
+    
 
 
-loadMeshToSemanticDic()
 
-# start_time = time.time()
-# process_articles_sequential("data//phase1","data//phase2")
-# print("--- %s seconds ---" % (time.time() - start_time))
 
-start_time = time.time()
-process_articles_concurrent("data//phase1","data//phase2")
-print("--- %s seconds ---" % (time.time() - start_time))
+if __name__ == "__main__":
+    loadMeshToSemanticDic()
+
+    # start_time = time.time()
+    # process_articles_sequential("data//phase1","data//phase2")
+    # print("--- %s seconds ---" % (time.time() - start_time))
+
+    start_time = time.time()
+    process_articles_concurrent("data//phase1","data//phase2")
+    print("--- %s seconds ---" % (time.time() - start_time))
